135697b453602571b4a119bb28ae29fe021f033f
Amirr <107915452+USER0106@users.noreply.github.com>
2022-07-30 15:05:11 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-//SCRIPT RECODE BY FANGZ 💌
+//SCRIPT RECODE BY AMIRR 💌
 // JANGAN DI HAPUS📣
 
-//BUY NO ENC? CHAT wa.me/6288215689772
-//GITHUB: https://github.com/Fangzbotz2007
+//BUY NO ENC? CHAT wa.me/6282154156241
+//GITHUB: https://github.com/USER0106
 
 //GA TAU CARA EDIT NYA? CHAT AUTHOR↑↑↑
 
@@ -36,33 +36,33 @@ let wibh = moment.tz('Asia/Jakarta').format('HH')
 // Kalau gapunya di biarin aja!, atau ga di kasih tanda '-'
 
 // kalo ga punya ketik "-" atau biarin aja biar ada creditsnya :v
-global.sig = 'https://instagram.com/fangzganz?igshid=YmMyMTA2M2Y=' //Instagram mu
-global.sgh = 'https://github.com/Fangzbotz2007' //Github mu
-global.sgc = 'https://chat.whatsapp.com/HpxyS6b45SoGIKG7LHddiM' //Group WhatsApp mu
-global.sdc = 'https://youtube.com/channel/UCACHvReRmw2fxgMutPFCBWg' //Discord Mu
+global.sig = 'https://instagram.com/AmirrWibuu_01=' //Instagram mu
+global.sgh = 'https://github.com/USER0106' //Github mu
+global.sgc = 'https://lynk.id/amirr' //Group WhatsApp mu
+global.sdc = 'https://lynk.id/amirr' //Discord Mu
 global.snh = 'https://nhentai.net/g/365296/' //Make ini ana gausah di ganti.
 
 /*⫘⫘⫘⫘ALL PAYMENT⫘⫘⫘⫘⫘*/
-global.pdana = '088215689772' //Dana
-global.povo = '088215689772' //Ovo
-global.pgopay = '088215689772' //Gopay
-global.ppulsa = '088215689772' //Pulsa 
+global.pdana = '082154156241' //Dana
+global.povo = '082154156241' //Ovo
+global.pgopay = '.082154156241' //Gopay
+global.ppulsa = '082154156241' //Pulsa 
 global.ppulsa2 = '0895394988123' //Pulsa, Kalau nomor cuma 1, yg ini kasih tanda -
-global.psaweria = 'https://saweria.co/FangzBot' //Saweria
+global.psaweria = 'https://lynk.id/amirr' //Saweria
 
 /*⫘⫘⫘⫘ GLOBAL NOMOR ⫘⫘⫘⫘⫘*/
-global.nomorbot = '62895394988123' //Nomor Bot mu
-global.nomorown = '6288215689772' //Nomormu
-global.namebot = 'Fangz BOT' //Nama bot mu
-global.nameown = 'Fangz Ganz' //Nama mu
+global.nomorbot = '6285849337475' //Nomor Bot mu
+global.nomorown = '6282154156241' //Nomormu
+global.namebot = 'AMERR BOT' //Nama bot mu
+global.nameown = 'Amirr' //Nama mu
 
 
 /*⫘⫘⫘⫘ STAFF ⫘⫘⫘⫘⫘*/
 global.owner = [
-  ['6288215689772'], //Owner1
-  ['6288215689772'], //Owner2
-  ['6288215689772'], //Owner3
-  ['6288215689772', 'FANGZ ! 🥶', true] //Ini buat kamu!
+  ['6282154156241'], //Owner1
+  ['6282154156241'], //Owner2
+  ['6282154156241'], //Owner3
+  ['6282154156241', 'AMIRR ! 🥶', true] //Ini buat kamu!
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -111,13 +111,13 @@ global.APIKeys = { // APIKey Here
 
 /*⫘⫘⫘⫘ WATERMAK ⫘⫘⫘⫘⫘*/
 //GANTI JANGAN NGASAL.
-global.wm = '                「 Fangz BOT ⁩இ 」' //Wm
-global.wm2 = '➳༻❀Fangz - Ganz❁ུ۪۪.;:୭̥.' //Name Wm
-global.wm3 = '⫹⫺ Fangz 𝗕𝗢𝗧'
+global.wm = '                「 AMIRR ⁩இ 」' //Wm
+global.wm2 = '➳༻❀AMIRR - GANZ❁ུ۪۪.;:୭̥.' //Name Wm
+global.wm3 = '⫹⫺ AMERR 𝗕𝗢𝗧'
 global.botdate = `⫹⫺ 𝗗𝗮𝘁𝗲: ${week} ${date}`
 global.bottime = `𝗧 𝗜 𝗠 𝗘 : ${wktuwib}`
-global.titlebot = '𝗬𝗧: FANGZ BOT' //Titlenya
-global.author = '                ✧ Fangz BOT⁩ ✧' //Wm author
+global.titlebot = '𝗬𝗧: AMIRR' //Titlenya
+global.author = '                ✧ AMERR BOT⁩ ✧' //Wm author
 
 
 /*⫘⫘⫘⫘ LOGO ⫘⫘⫘⫘⫘*/
@@ -171,7 +171,7 @@ global.hsquere = ['⛶','❏','⫹⫺']
 
 /*⫘⫘⫘⫘ STICKER ⫘⫘⫘⫘⫘*/
 global.stickpack = '.'
-global.stickauth = `© Fangz BOT\nOfficial by Whatsapp\nNomor: ${global.nomorbot}`
+global.stickauth = `© AMIRR\nOfficial by Whatsapp\nNomor: ${global.nomorbot}`
 
 global.multiplier = 38 // The higher, The harder levelup
 
